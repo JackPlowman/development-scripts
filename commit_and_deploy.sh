54830cb95751e8ec7c21e9f0cd19067bf5d62244
JackPlowman <62281988+JackPlowman@users.noreply.github.com>
2024-03-21 08:21:41 +0000

    Default commit message

--- commit_and_deploy.sh
+++ commit_and_deploy.sh
@@ -6,11 +6,15 @@ commit_title="${1:-Default commit message}"
 # Commit the changes with the provided commit title
 git commit -m "$commit_title"
 
-# Push the changes to the remote repository
-git push
+# # Push the changes to the remote repository
+# git push
 
-# Deploy the changes to the AWS with GitHub Actions
-gh workflow run .... --field key=value
+# # Deploy the changes to the AWS with GitHub Actions
+# gh workflow run .... --field key=value
 
-# Wait for the deployment to finish
-gh run watch <run-id> [flags]
+# #
+# open ...
+
+# # Wait for the deployment to finish
+# gh run watch <run-id> [flags]
+
